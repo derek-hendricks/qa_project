7ca6adb79e0c765757b677e507912c09c16a84ed
Maria Diaz <mariadiaz@Marias-MacBook-Air-2.local>
2022-03-07 19:03:37 -0800

    more tests

--- __tests__/pageObjects.ts
+++ __tests__/pageObjects.ts
@@ -4,19 +4,28 @@ import {BasePage} from "./basePage"
 export class Bestbuy extends BasePage {
     searchBar: By = By.className("search-input");
     results: By = By.xpath('//div[@class="app-container lv"]');
+    searchButton: By = By.xpath('//button[@class="header-search-button"]');
     userMenu: By = By.xpath('//button[@class="c-button-unstyled plButton account-button d-flex justify-content-center align-items-center"]');
     signIn: By = By.xpath('//a[@class="c-button c-button-secondary c-button-sm sign-in-btn"]');
-    createAccount: By = By.xpath('//a[@class="c-button c-button-outline c-button-sm create-account-btn"]');
     emailSignIn: By = By.xpath('//input[@type="email"]');
     passwordSignIn: By = By.xpath('//input[@type="password"]');
     loginButton: By = By.xpath('//button[@type="submit"]');
     closePopup: By = By.xpath('//button[@class="c-close-icon c-modal-close-icon"]');
-    
+    addToCartButton: By = By.className("fulfillment-add-to-cart-button");
+    cartButton: By = By.xpath('//div[@class="go-to-cart-button"]');
+    menu: By = By.xpath('//button[@class="c-button-unstyled hamburger-menu-button"]');
+    appliances: By = By.xpath('//button[@data-id="node-197"]');
+    allAppliances: By = By.xpath('//a[@data-lid="ubr_app_dept"]');
+    appliancePage: By = By.xpath('//div[@class="vn-panel col-xs-6 vn-panel-1"]');
+
     constructor() {
         super({url: "https://www.bestbuy.com/"});
     }
     async search(searchTerm: string) {
         return this.setInput(this.searchBar, `${searchTerm}\n`)
+    }
+    async clickSearch () {
+        return this.click(this.searchButton)
     }
     async getResults () {
         return this.getText(this.results)
@@ -30,4 +39,20 @@ export class Bestbuy extends BasePage {
     async closeWindow () {
         return this.click(this.closePopup)
     }
+    async clickMenu () {
+        return this.click(this.menu)
+    }
+    async clickAppliances () {
+        return this.click(this.appliances)
+    }
+    async clickAllAppliances () {
+        return this.click(this.allAppliances)
+    }
+    async appliancesResults () {
+        return this.getText(this.appliancePage)
+    }
+    async clickAddToCart () {
+        return this.click(this.addToCartButton)
+    }
+
 }
---
+++ __tests__/testFile.test.ts
@@ -0,0 +1,31 @@
+import { Bestbuy } from "./pageObjects";
+
+const bestbuy = new Bestbuy;
+
+afterAll(async () => {
+    await bestbuy.driver.quit();
+});    
+//will input log in test before presentation -md
+//if we run a log in too many times we get a password error -md
+test ("Can get to log in page", async () => {
+    await bestbuy.navigate();
+    await bestbuy.closeWindow();
+    await bestbuy.openMenu();
+    await bestbuy.clickSignIn();
+})
+test ("can use menu categories", async () => {
+    await bestbuy.navigate();
+    await bestbuy.clickMenu();
+    await bestbuy.clickAppliances();
+    await bestbuy.clickAllAppliances();
+    expect (await bestbuy.appliancesResults()).toContain("Refrigerators")
+})
+test ("Can do a search", async () => {
+    await bestbuy.navigate();
+    await bestbuy.search("macbook");
+    await bestbuy.clickSearch();
+    expect (await bestbuy.getResults()).toContain("Apple")
+})
+test ("Can add to cart", async () => {
+    await bestbuy.clickAddToCart();
+})
