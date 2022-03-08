2514b7b0cbd4a87e1aacb3bc2e7bd24645b07094
Derek Hendricks <derek.m.hendricks>
2022-03-07 22:42:30 -0700

    edit

--- __tests__/testFile.test.ts
+++ __tests__/testFile.test.ts
@@ -5,37 +5,35 @@ const bestbuy = new Bestbuy;
 
 afterAll(async () => {
     await bestbuy.driver.quit();
-});    
+});
 //will input log in test before presentation -md
 //if we run a log in too many times we get a password error -md
-test ("Can get to log in page", async () => {
+test("Can get to log in page", async () => {
     await bestbuy.navigate();
-    await bestbuy.click(bestbuy.americaLink);
+    // uncomment when running outside of US
+    // await bestbuy.click(bestbuy.americaLink);
     await bestbuy.closeWindow();
     await bestbuy.openMenu();
     await bestbuy.clickSignIn();
 })
-test ("can use menu categories", async () => {
+test("can use menu categories", async () => {
     await bestbuy.navigate();
     await bestbuy.clickMenu();
     await bestbuy.clickAppliances();
     await bestbuy.clickAllAppliances();
-    expect (await bestbuy.appliancesResults()).toContain("Refrigerators")
+    expect(await bestbuy.appliancesResults()).toContain("Refrigerators")
 })
-test ("Can do a search", async () => {
+test("Can do a search", async () => {
     await bestbuy.navigate();
     await bestbuy.search("macbook");
     await bestbuy.clickSearch();
-    expect (await bestbuy.getResults()).toContain("Apple")
+    expect(await bestbuy.getResults()).toContain("Apple")
 })
-test ("Can add to cart", async () => {
+test("Can add to cart", async () => {
     await bestbuy.clickAddToCart();
     await bestbuy.clickGoToCart();
-    expect (await bestbuy.viewCart()).toContain("MacBook")
+    expect(await bestbuy.viewCart()).toContain("MacBook")
 })
-
-
-
 
 test("Can find search results for an item entered in the search bar ", async () => {
     await bestbuy.navigate();
@@ -53,7 +51,7 @@ test("Can save an item from the search results", async () => {
     await bestbuy.clickSearch();
     await bestbuy.click(bestbuy.saveItemButton);
     await bestbuy.click(bestbuy.savedItems);
-    expect (await bestbuy.getText(bestbuy.savedItemsResults)).toContain("Xbox");
+    expect(await bestbuy.getText(bestbuy.savedItemsResults)).toContain("Xbox");
 });
 
 test("Can compare an item from the search results", async () => {
@@ -62,6 +60,6 @@ test("Can compare an item from the search results", async () => {
     await bestbuy.search("xbox");
     await bestbuy.clickSearch();
     await bestbuy.click(bestbuy.compareCheckbox);
-    expect (await bestbuy.getText(bestbuy.comparison)).toContain("Compare");
+    expect(await bestbuy.getText(bestbuy.comparison)).toContain("Compare");
 });
 
