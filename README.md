328e7102f794aed87bec349f1bd2bf5f06e50515
Derek Hendricks <derek.m.hendricks>
2022-03-15 02:29:14 -0600

    readme and tests edit

---
+++ README.md
@@ -0,0 +1,8 @@
+<h2>Automated tests for bestbuy.com using Selenium, Jest, Node, Typescript </h2>
+
+<h3>Install:</h3>
+- git clone https://github.com/derek-hendricks/qa_project.git <br/>
+- npm i 
+
+<h3>Run Tests:</h3>
+- npm run test testFile
--- __tests__/testFile.test.ts
+++ __tests__/testFile.test.ts
@@ -5,22 +5,26 @@ const bestbuy = new Bestbuy;
 afterAll(async () => {
   await bestbuy.driver.quit();
 });
-//caution: if we run log in too many times we get a password error
-/*test("Can login and logout of an account", async () => {
+
+/*test("Can open login and logout of an account", async () => {
   await bestbuy.navigate();
   // when running outside of US
   await bestbuy.goToAmericanSite();
-  //await bestbuy.closeWindow();
+  await bestbuy.closeWindow();
   await bestbuy.openMenu();
   await bestbuy.clickSignIn();
-  await bestbuy.email("qa_group2@yahoo.com")
-  await bestbuy.password("PleaseWork!")
+  // Info: need to create an email to replace test_email@yahoo.com 
+  // await bestbuy.email("test_email@yahoo.com")
+  // password for bestbuy account
+  // await bestbuy.password("test_password")
   await bestbuy.confirmLogin();
   await bestbuy.openMenu();
   await bestbuy.clickLogout();
 })*/
 test("can use menu categories", async () => {
   await bestbuy.navigate();
+  // when running outside of US
+  await bestbuy.goToAmericanSite();
   await bestbuy.clickMenu();
   await bestbuy.clickAppliances();
   await bestbuy.clickAllAppliances();
@@ -37,10 +41,10 @@ test("Can add to cart", async () => {
   await bestbuy.clickGoToCart();
   expect(await bestbuy.viewCart()).toContain("The Sims 4")
 })
-test ("can open checkout page", async () => {
+test("can open checkout page", async () => {
   await bestbuy.clickCheckOut();
   await bestbuy.clickContinueAsGuest();
-  expect (await bestbuy.checkOutResults()).toContain("The Sims 4")
+  expect(await bestbuy.checkOutResults()).toContain("The Sims 4")
 })
 
 test("Can save an item from the search results", async () => {
@@ -53,17 +57,15 @@ test("Can save an item from the search results", async () => {
 });
 
 test("Can compare two items from the search results", async () => {
-  const xboxSearch: string = "Xbox";
   await bestbuy.navigate();
-  await bestbuy.search(xboxSearch);
+  await bestbuy.search("xbox");
   await bestbuy.clickSearch();
   await bestbuy.clickCompareCheckbox(bestbuy.compareCheckboxOne);
-  expect(await bestbuy.getText(bestbuy.comparison)).toContain("Compare (1)");
   await bestbuy.clickCompareCheckbox(bestbuy.compareCheckboxTwo);
   expect(await bestbuy.getText(bestbuy.comparison)).toContain("Compare (2)");
   await bestbuy.viewComparedResults();
   let compareResults = await bestbuy.getText(bestbuy.compareResults);
-  expect(compareResults).toContain(xboxSearch);
+  expect(compareResults).toContain("Xbox");
   expect(compareResults).toContain("All Specs");
 });
 
